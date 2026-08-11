4907ed8eaea3aa077e66c86b1b0dee7f8098ebfa
Manuel Hanono <mhanono@google.com>
2026-08-11 14:29:44 +0000

    Add Vertex AI Agent Builder Data Store and Chat Engine Terraform

---
+++ modules/data_redaction_nyl_poc/agent.tf
@@ -0,0 +1,32 @@
+# Vertex AI Agent Builder (Search and Conversation)
+# 1. The Data Store that will hold the redacted documents
+resource "google_discovery_engine_data_store" "redacted_data_store" {
+  location                    = "global"
+  data_store_id               = "redacted-docs-store"
+  display_name                = "Redacted Unstructured Documents"
+  industry_vertical           = "GENERIC"
+  content_config              = "CONTENT_REQUIRED"
+  solution_types              = ["SOLUTION_TYPE_CHAT"]
+  project                     = var.project_id
+  create_advanced_site_search = false
+}
+
+# 2. The Chat Agent built on top of the Data Store
+resource "google_discovery_engine_chat_engine" "redacted_agent" {
+  location          = "global"
+  engine_id         = "redacted-docs-agent"
+  collection_id     = "default_collection"
+  display_name      = "Redacted Docs Agent"
+  industry_vertical = "GENERIC"
+  project           = var.project_id
+  
+  data_store_ids = [google_discovery_engine_data_store.redacted_data_store.data_store_id]
+  
+  chat_engine_config {
+    agent_creation_config {
+      business              = "New York Life"
+      default_language_code = "en"
+      time_zone             = "America/New_York"
+    }
+  }
+}
