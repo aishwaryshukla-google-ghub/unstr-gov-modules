b49cc13038a7bb931b13092b53b74df713279122
Manuel Hanono <mhanono@google.com>
2026-08-19 14:35:45 +0000

    Remove Dialogflow API enablement as it is already enabled

--- modules/data_redaction_nyl_poc/agent.tf
+++ modules/data_redaction_nyl_poc/agent.tf
@@ -1,10 +1,3 @@
-# Enable Dialogflow API required by Chat Engine
-resource "google_project_service" "dialogflow" {
-  project            = var.project_id
-  service            = "dialogflow.googleapis.com"
-  disable_on_destroy = false
-}
-
 resource "random_id" "vertex_suffix" {
   byte_length = 3
 }
@@ -19,8 +12,6 @@ resource "google_discovery_engine_data_store" "redacted_data_store" {
   solution_types              = ["SOLUTION_TYPE_CHAT"]
   project                     = var.project_id
   create_advanced_site_search = false
-
-  depends_on = [google_project_service.dialogflow]
 }
 
 # 2. The Chat Agent built on top of the Data Store
@@ -33,8 +24,6 @@ resource "google_discovery_engine_chat_engine" "redacted_agent" {
   project           = var.project_id
 
   data_store_ids = [google_discovery_engine_data_store.redacted_data_store.data_store_id]
-
-  depends_on = [google_project_service.dialogflow]
   
   chat_engine_config {
     agent_creation_config {
